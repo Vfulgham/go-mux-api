0eb55195a4f61dc84fd91f3144b24b6081cd9238
Vicki <vicki@Stevens-MacBook-Pro.local>
2022-12-09 16:01:53 -0500

    pushing docker-compose

---
+++ pkg/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+    "log"
+
+    "github.com/tutorials/go/crud/pkg/models"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+)
+
+func Init() *gorm.DB {
+
+    // connection string, really should be calling from .env file
+    dbURL := "postgres://pg:pass@localhost:5432/crud"
+
+    // open connection to Postgres db using dbURL
+    db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    // migrate models
+    db.AutoMigrate(&models.Book{})
+
+    return db
+}

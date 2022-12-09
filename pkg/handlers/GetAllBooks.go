6c9f78601be42c75407621c98945f536ad501230
Vicki <vicki@Stevens-MacBook-Pro.local>
2022-12-09 16:18:07 -0500

    committing

--- pkg/handlers/GetAllBooks.go
+++ pkg/handlers/GetAllBooks.go
@@ -11,4 +11,5 @@ func (h handler) GetAllBooks(w http.ResponseWriter, r *http.Request){
 	w.Header().Add("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(mocks.Books)
+	
 }
